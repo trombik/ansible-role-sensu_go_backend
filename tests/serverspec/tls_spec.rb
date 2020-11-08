9e85140f6c8e01844f86b08da885466ccffdee40
Tomoyuki Sakurai <y@trombik.org>
2020-11-09 05:21:00 +0900

    bugfix: fix examples in TLS spec

--- tests/serverspec/tls_spec.rb
+++ tests/serverspec/tls_spec.rb
@@ -36,8 +36,8 @@ end
 ports_http.each do |port|
   describe command "(echo 'GET / HTTP/1.0'; echo; sleep 1) | openssl s_client -connect 127.0.0.1:#{port} -servername localhost -CAfile #{ca_cert}" do
     its(:exit_status) { should eq 0 }
-    its(:stdout) { should match(/issuer=CN = Sensu Test CA/) }
-    its(:stdout) { should match(/subject=CN = localhost/) }
+    its(:stdout) { should match(%r{issuer=CN = Sensu Test CA|issuer=/CN=Sensu Test CA}) }
+    its(:stdout) { should match(%r{subject=CN = localhost|subject=/CN=localhost}) }
     its(:stdout) { should match(/-----BEGIN CERTIFICATE-----/) }
     its(:stdout) { should match(%r{HTTP/1\.0 \d{3}}) }
   end
